67bc1ac2a755f081f0245908d243414f589bc222
Shreya <123@gmail.com>
2020-10-31 16:49:41 -0500

    Animations of Characters

--- sketch.js
+++ sketch.js
@@ -1,6 +1,19 @@
+var safePlayer1, safePlayer2, safePlayer1Img, safePlayer2Img;
+
+function preload() {
+  safePlayer1Img = loadImage("boy.jpeg");
+  safePlayer2Img = loadImage("girl.jpeg");
+}
+
 function setup() {
   createCanvas(800,400);
-  createSprite(400, 200, 50, 50);
+  safePlayer1 = createSprite(200, 200, 50, 50);
+  safePlayer1.addImage(safePlayer1Img);
+  safePlayer1.scale = 0.2
+
+  safePlayer1 = createSprite(400, 200, 50, 50);
+  safePlayer1.addImage(safePlayer2Img);
+  safePlayer1.scale = 0.2
 }
 
 function draw() {
