7b8933be3bc6f35d6c55d1d3d7b2feecbd1277a1
Shreya <123@gmail.com>
2020-11-24 18:13:44 -0600

    Game

---
+++ extraObjects.js
@@ -0,0 +1,21 @@
+//Health bar - player & Infector
+function Health(){
+
+}
+
+//Shield/Immunity
+function Shield(){
+
+}
+
+//Invisibility
+function Invisibility() {
+
+}
+
+//weapons-masks, gloves, sanitizers, face shields, 
+function weapons() {
+    
+}
+
+//
--- sketch.js
+++ sketch.js
@@ -1,22 +1,58 @@
-var safePlayer1, safePlayer2, safePlayer1Img, safePlayer2Img;
+var Player, Player2, PlayerImg, Player2Img;
+var Infector;
 
 function preload() {
-  safePlayer1Img = loadImage("boy.jpeg");
-  safePlayer2Img = loadImage("girl.jpeg");
+  PlayerImg = loadImage("boy.png");
+  //Player2Img = loadImage("girl.png");
 }
 
 function setup() {
-  createCanvas(800,400);
-  safePlayer1 = createSprite(200, 200, 50, 50);
-  safePlayer1.addImage(safePlayer1Img);
-  safePlayer1.scale = 0.2
+  createCanvas(1250,600);
+  Player = createSprite(50, 200, 50, 50);
+  Player.addImage(PlayerImg);
+  Player.scale = 0.2
 
-  safePlayer1 = createSprite(400, 200, 50, 50);
-  safePlayer1.addImage(safePlayer2Img);
-  safePlayer1.scale = 0.2
+
+  Infector = createSprite(400, 200, 50, 50);
+  //Infector.addImage(InfectorImg);
+  Infector.scale = 0.2
 }
 
 function draw() {
-  background(255,255,255);  
+  background(0);  
+
+  /*** CONTROLS - PLAYER 1  - ARROW KEYS*/
+  if(keyDown(RIGHT_ARROW)) {
+    Player.x = Player.x+4;
+  }
+  if(keyDown(LEFT_ARROW)) {
+    Player.x = Player.x-4;
+  }
+  if(keyDown(UP_ARROW)) {
+    Player.y = Player.y-4;
+  }
+  if(keyDown(DOWN_ARROW)) {
+    Player.y = Player.y+4;
+  }
+
+  camera.position.x = Player.x + 400;
+
+  /*
+  /*** CONTROLS - PLAYER 2  - AWSD
+  if(keyDown("d")) {
+    Player2.x = Player2.x+4;
+  }
+  if(keyDown("a")) {
+    Player2.x = Player2.x-4;
+  }
+  if(keyDown("w")) {
+    Player2.y = Player2.y-4;
+  }
+  if(keyDown("s")) {
+    Player2.y = Player2.y+4;
+  }
+  */
+
+
   drawSprites();
 }
